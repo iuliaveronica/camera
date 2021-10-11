5cadd1c49778cff3166f92ffcc7befb42d5a6d52
iuliaveronica <92303749+iuliaveronica@users.noreply.github.com>
2021-10-11 13:05:27 +0300

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,9 +1,9 @@
 var video=document.getElementById("video");
 function on_cam_success(stream){
-video scrObject=stream;
+video srcObject=stream;
 }function on_cam_error(err)
 {
-alert(error."+err.message);
+alert("error."+err.message);
 }
 var constraints={ audio: false, video: true };
 navigator.mediaDevices.getUserMedia(constraints)
@@ -12,8 +12,8 @@ navigator.mediaDevices.getUserMedia(constraints)
 function capteaza()
 {
 var c=document.getElementById("canvas");
-c.width+video.width;
-c.height+video.height;
+c.width=video.width;
+c.height=video.height;
 var ctx=c.getContext("2d");
 ctx.drawImage(video, 0, 0, 640, 480);
 }
